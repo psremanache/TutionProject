8906937951ae5cb4e55e4cb4304b86d0a96de09d
Prashant Remanache <psremanache6@gmail.com>
2025-02-15 20:40:09 +0530

    added infrastructure project and included othe projects

---
+++ EF/EntityFrameworkCore.Infrastructure/TutionRepository.cs
@@ -0,0 +1,13 @@
+﻿using EntityFrameworkCore.Domain.RepositoryInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkCore.Infrastructure
+{
+    public class TutionRepository:ITutionRepository
+    {
+    }
+}
