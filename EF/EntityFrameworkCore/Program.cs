652f269364af66d363504677f99aed1701cf1bc8
Prashant Remanache <psremanache6@gmail.com>
2025-02-15 23:45:52 +0530

    added dependency injection of services of infrastructure and applicaion layer

--- EF/EntityFrameworkCore/Program.cs
+++ EF/EntityFrameworkCore/Program.cs
@@ -1,6 +1,8 @@
 
+using EntityFrameworkCore.Application.RegisterService;
 using EntityFrameworkCore.Entities;
 using EntityFrameworkCore.Infrastructure.Data;
+using EntityFrameworkCore.Infrastructure.RegisterService;
 using EntityFrameworkCore.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
@@ -15,49 +17,15 @@ namespace EntityFrameworkCore
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            .AddJwtBearer(options =>
-            {
-                var jwtSettings = builder.Configuration.GetSection("JwtSettings");
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidateLifetime = true,
-                    ValidateIssuerSigningKey = true,
-                    ValidIssuer = jwtSettings["Issuer"],
-                    ValidAudience = jwtSettings["Issuer"],
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtSettings["Secret"]))
-                };
-                options.Events = new JwtBearerEvents
-                {
-                    OnAuthenticationFailed = context =>
-                    {
-                        // Log the error message here
-                        Console.WriteLine("Authentication failed: " + context.Exception.Message);
-                        return Task.CompletedTask;
-                    }
-                };
-
-            });
-
-            builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
-
-            // Register JwtService
-            builder.Services.AddSingleton<JwtService>();
-            builder.Services.AddAuthorization(options =>
-            {
-                options.AddPolicy("Student", policy => policy.RequireRole("Student"));
-                options.AddPolicy("Admin", policy => policy.RequireRole("Admin"));
-                options.AddPolicy("Instructor", policy => policy.RequireRole("Instructor"));
-                options.AddPolicy("AllPolicy", policy => policy.RequireRole("Student","Instructor","Admin"));
-            });
-            var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+            var conString = builder.Configuration.GetConnectionString("DBcon");
+            var jwtSetting = builder.Configuration.GetSection("JwtSettings");
+            builder.Services.AddApplication(jwtSetting);
+            
+            //var jwtSettings = builder.Configuration.GetSection("JwtSettings");
 
             // Add services to the container.
-            builder.Services.AddDbContext<DataContext>(options =>
-            options.UseSqlServer(builder.Configuration.GetConnectionString("DBcon")).LogTo(Console.WriteLine));
             builder.Services.AddControllers();
+            builder.Services.AddInfrastructure(conString);
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen(opt =>
