dcd140e70dd966688215dfb4f864b9a7b26f0be8
Prashant Remanache <psremanache6@gmail.com>
2025-02-15 21:16:52 +0530

    added login repository

--- EF/EntityFrameworkCore.Infrastructure/Repositories/LoginRepository.cs
+++ EF/EntityFrameworkCore.Infrastructure/Repositories/LoginRepository.cs
@@ -1,4 +1,9 @@
 ﻿using EntityFrameworkCore.Domain.RepositoryInterfaces;
+using EntityFrameworkCore.Entities;
+using EntityFrameworkCore.Infrastructure.Data;
+using EntityFrameworkCore.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +14,40 @@ namespace EntityFrameworkCore.Infrastructure.Repositories
 {
     public class LoginRepository : ILoginRepository
     {
+        private readonly DataContext _dataContext;
+        private readonly JwtService _jwtService;
+        public LoginRepository(DataContext dataContext, JwtService jwtService)
+        {
+            _dataContext = dataContext;
+            _jwtService = jwtService;
+        }
+        public async Task<string> Login([FromBody] LoginRequest request)
+        {
+            var userWithRole = await _dataContext.Users
+            .Where(x => x.Username == request.Username && x.Password == request.Password)
+            .Select(user => new User
+            {
+                Id = user.Id,
+                Username = user.Username,
+                Role = _dataContext.Roles
+                    .Where(role => role.RoleId == user.RoleId)
+                    .Select(role => role)
+                    .FirstOrDefault()
+            })
+            .FirstOrDefaultAsync();
+
+            if (userWithRole == null)
+                return string.Empty;
+
+            var token = _jwtService.GenerateToken(userWithRole.Username, userWithRole.Role.RoleName);
+            return token;
+        }
+
+        public async Task<User> Register([FromBody] User user)
+        {
+            _dataContext.Users.Add(user);
+            await _dataContext.SaveChangesAsync();
+            return user;
+        }
     }
 }
